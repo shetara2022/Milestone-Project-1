cf9116afe53743efa255351a9a23a5b89896bd59
shetara2022 <smithshetarall@gmail.com>
2022-07-21 21:38:28 -0400

    added questions to js and some css styling

---
+++ project1.js
@@ -0,0 +1,45 @@
+
+//display questions in a div or section
+//user needs to able to select the correct answer (event listener/click)
+//if answer is correct, move to next question
+//else give correct answer 
+//add points if answer is correct 
+
+
+let questions = [
+    {question: 'What is Methodology the study of?',
+option1: 'space',
+option2: 'methods',
+option3: 'tigers',
+option4: 'dreams',
+answer: 2,
+},
+
+{question: 'How many years is one score?',
+option1: '20',
+option2: 'methods',
+option3: 'tigers',
+option4: 'dreams', 
+answer: 1,
+},
+
+{question: 'What did Peter Piper pick?',
+option1: 'a bunch of bananas',
+option2: 'a bushel of flowers',
+option3: 'a peck of pickled peppers',
+option4: 'his nose',
+answer: 3,
+},
+
+{question: 'What is the capital of North Carolina?',
+option1: 'Charlotte',
+option2: 'Greensboro',
+option3: 'Asheville',
+option4: 'Raleigh',
+answer: 4,
+},
+]
+
+
+
+//
