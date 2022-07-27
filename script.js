ad0b3e006a726caf04484a2041e4cd4f04b1121c
shetara2022 <smithshetarall@gmail.com>
2022-07-26 22:13:59 -0400

    editted index.html to specify/clarify div elements and deleted incorrect function codes from script.js

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-//first question 
-//next function
-//
-//set timer 
+//select questions
+const questionContainer = document.getElementById('question-container');
+const getQuestions = document.querySelector('question');
+const options = document.querySelectorAll('btn');
+const currectQuestion = [];
+const questionCounter = 0;
+const maxQuestions = 5; 
+
+//question bank
+const questions = [
+    {
+      question: 'What is Methodology the study of?',
+      answer: [ 'space', 'methods', 'tigers', 'dreams'],
+      correctAnswer: 'methods'
+},
+
+{
+  question: 'How many years is one score?',
+  answer: ['20', '50', '15', '5'],
+  correctAnswer: '20'
+},
+
+{
+  question: 'What did Peter Piper pick?',
+  answer: [ 'a bunch of bananas', 'a bushel of flowers', 'a peck of pickled peppers','his nose'], 
+  correctAnswer: 'a peck of pickled peppers'
+},
+
+{
+  question: 'What is the capital of North Carolina?',
+  answer: ['Charlotte', 'Greensboro', 'Asheville', 'Raleigh'],
+  correctAnswer: 'Raleigh'
+},
+]
+//function to show questions
+function showQuestion(){
+  display.innerText = getQuestions.question;
+}
+
+
+//set timer (https://www.codegrepper.com/code-examples/javascript/add+countdown+timer+to+javascript+quiz)
 var count = 15;
 var interval = setInterval(function(){
   document.getElementById('countdown-timer').innerHTML=count;
@@ -11,43 +48,3 @@ var interval = setInterval(function(){
     document.getElementById('countdown-timer').innerHTML='Time is up!'
   }
 }, 1000);
-
-
-// function
-//questions 
-const questions = [
-    {question: 'What is Methodology the study of?',
-option1: 'space',
-option2: 'methods',
-option3: 'tigers',
-option4: 'dreams',
-answer: 2,
-},
-
-{question: 'How many years is one score?',
-option1: '20',
-option2: 'methods',
-option3: 'tigers',
-option4: 'dreams', 
-answer: 1,
-},
-
-{question: 'What did Peter Piper pick?',
-option1: 'a bunch of bananas',
-option2: 'a bushel of flowers',
-option3: 'a peck of pickled peppers',
-option4: 'his nose',
-answer: 3,
-},
-
-{question: 'What is the capital of North Carolina?',
-option1: 'Charlotte',
-option2: 'Greensboro',
-option3: 'Asheville',
-option4: 'Raleigh',
-answer: 4,
-},
-]
-
-
-
