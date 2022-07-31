a675a41722b6ce846e27935184ef8e8728c642d0
shetara2022 <smithshetarall@gmail.com>
2022-07-31 14:08:21 -0400

    rewrote script.js to i,prove functionality, added question counter and accurate countdown in js

--- script.js
+++ script.js
@@ -1,98 +1,204 @@
-//const getQuestion = document.getElementById("question");        //reference my questions and answer choices
-//console.log(getQuestion);
-const answerButtons = document.querySelectorAll(".answer");
-//console.log(answerButtons);
-const nextButton = document.getElementById('next-btn');
-const totalQuestions = 5; //how many questions to answer
-
-
-let  currentQuestion = [];      //select current question
-let availableQuestion = [];    //points to how many questions left 
-let questionCounter = 0;       //count the question you are on
-let score = 0;                //how many question answered correctly
-
-
-const questions = [                 //QUESTION BANK
+const questionBank = [           //QUESTION BANK
   {
     question: 'What is Methodology the study of?',
-    answer: [ 'space', 'methods', 'tigers', 'dreams'],
-    correctAnswer: 'methods'
-},
+    option1: 'space',
+    option2: 'methods',
+    option3: 'tigers',
+    option4: 'dreams',
+    correctAnswer: '2'
+  },
 
-{
-question: 'How many years is one score?',
-answer: ['20', '50', '15', '5'],
-correctAnswer: '20'
-},
+  {
+    question: 'How many years is one score?',
+    option1: '20',
+    option2: '50',
+    option3: '15',
+    option4:  '5',
+    correctAnswer: 1,
+  },
 
-{
-question: 'What did Peter Piper pick?',
-answer: [ 'a bunch of bananas', 'a bushel of flowers', 'a peck of pickled peppers','his nose'], 
-correctAnswer: 'a peck of pickled peppers'
-},
+  {
+    question: 'What did Peter Piper pick?',
+    option1: 'a bunch of bananas',
+    option2: 'a bushel of flowers',
+    option3: 'a peck of pickled peppers',
+    option4: 'his nose',
+    correctAnswer: 3,
+  },
 
-{
-question: 'What is the capital of North Carolina?',
-answer: ['Charlotte', 'Greensboro', 'Asheville', 'Raleigh'],
-correctAnswer: 'Raleigh'
-},
+  {
+    question: 'What is the capital of North Carolina?',
+    option1: 'Charlotte',
+    option2: 'Greensboro',
+    option3:  'Asheville',
+    option4:  'Raleigh',
+    correctAnswer: 4,
+  },
 
-{
-  question: 'What is the most common species of bird in the world?',
-answer: ['pigeon', 'duck', 'chicken', 'blue jay'],
-correctAnswer: 'chicken'
-},
+  {
+    question: 'What is the most common species of bird in the world?',
+    option1: 'pigeon', 
+    option2: 'duck', 
+    option3:  'chicken', 
+    option4:  'blue jay',
+    correctAnswer: 3,
+  }
 ]
 
+const timer = document.getElementById('time');
+const question = document.getElementById('question');
+const answerChoice = Array.from(document.getElementsByClassName('answer'));
+// console.log(answerChoice);
+const nextButton = document.getElementById('next-btn');
+const scoreboard = document.getElementById('results');
+const totalQuestions = 5;
+const questionCounterText = document.querySelector('.hud-main-text');
 
-function displayQuestion() {         //display question ----select random questions???
-questionCounter++;
-const questionIndex = Math.floor(Math.random() * availableQuestion.length); //Code accessed from Stack Overflow(https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array)
-currentQuestion = availableQuestion[questionIndex];
-question.innerText = currentQuestion.question; //innerText renders only the text content of an element - (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText)
-// console.log(displayQuestion);
-
-//display answer choices 
-answerButtons.forEach(function(element,index){
-element.textContent = currentQuestion.answer[index];
-});
-  }
-
-
+let currentQuestion = {};
+let questionCounter = 0;
+let availableQuestions = [];
+let score = 0;
 
 
-//THIS FUNCTION STARTS THE QUESTION COUNTER AND SCOREBOARD FOR THE TRIVIA 
-function playGame() {
-questionCounter= 0;
-score = 0;
-availableQuestion = questions;
-//console.log(availableQuestion);
-displayQuestion();
+//after clicking start 
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questionBank];
+  displayQuestion();
 };
-playGame();
+
+//Show Question from question bank
+function displayQuestion() {
+  // if (availableQuestions.length === 0) {
+  //   //show scoreboard
+  // }
+  questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${totalQuestions}`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length); //Code accessed from Stack Overflow(https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array)
+  currentQuestion = availableQuestions[questionIndex];  //keeps track of which question we are on
+  question.innerText = currentQuestion.question; //innerText renders only the text content of an element - (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText)
+
+  //select the number from the dataset property for the corresponding question
+  answerChoice.forEach(option => {
+    const number = option.dataset['number'];
+    option.innerText = currentQuestion['option' + number];
+    // console.log(option);
+  });
+  
+  availableQuestions.splice(questionIndex, 1);
+};
+
+answerChoice.forEach(option => {
+  option.addEventListener('click', e => {
+    const answerSelected = e.target;
+    const selectedAnswer = answerSelected.dataset['number'];
+
+    let classToApply = 'incorrect';
+    if (selectedAnswer == currentQuestion.correctAnswer) {
+      classToApply = 'correct';
+    };
+    
+    answerSelected.parentElement.classList.add(classToApply);
+
+    setTimeout( () => {
+      answerSelected.parentElement.classList.remove(classToApply);
+      displayQuestion();
+    }, 2000);
+
+  });
+});
 
 
+startGame();
 
 
+// // BUTTONS 
+// const answerButton = document.querySelectorAll(".answer");
+// //When I click next button...
+//// let currentQuestion = {};      //select current question     const nextButton = document.getElementById('next-btn');
+// nextButton.addEventListener('click', displayQuestion);  
+
+// const scoreboard = document.getElementById('total_correct');
+// //const time = document.getElementById('time');
 
 
-// //activate the next button 
-nextButton.addEventListener('click', displayQuestion);
-
-
-//final page shows the results: "You scored -- out of --!" 
-
-
-
-
-
-//set timer (https://www.codegrepper.com/code-examples/javascript/add+countdown+timer+to+javascript+quiz)
-// var count = 15;
-// var interval = setInterval(function(){
-//   document.getElementById('countdown-timer').innerHTML=count;
+// //SET TIMER 
+// let count = 15;
+// let timer = setInterval(function(){
+//   document.getElementById('time').innerHTML=count;
 //   count--;
 //   if (count === 0){
-//     clearInterval(interval);
-//     // document.getElementById('countdown-timer').innerHTML='Time is up!'
+//     clearInterval(timer);
+//     document.getElementById('time').innerHTML = 'Time is up!';
+//     nextButton.click();
+//     displayQuestion()
 //   }
 // }, 1000);
+
+
+
+// function displayQuestion() {         //display random questions
+//   questionCounter++;
+//   const questionIndex = Math.floor(Math.random() * availableQuestion.length); //Code accessed from Stack Overflow(https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array)
+//   currentQuestion = availableQuestion[questionIndex];  //keeps track of which question we are on
+//   question.innerText = currentQuestion.question; //innerText renders only the text content of an element - (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText)
+
+//   answerButton.forEach(options => {
+//     const number = options.dataset['number'];
+//     options.innerText = currentQuestion["options" + number];
+//   });
+// answerButton.forEach(function (element, index) {
+//     element.innerText = currentQuestion.options[index];
+// });
+  
+//   availableQuestion.splice(questionIndex, 1);
+  
+//   correctAnswer = true;
+// };
+//     //add event listener to answer buttons
+//     answerButton.forEach(options => {
+//       options.addEventListener('click', e => {
+        
+//       })
+//     })
+//     // nextButton.click();
+//     //   timer = setInterval(timer, 1000);
+// displayQuestion();
+
+// // //correct answer vs incorrect 
+
+
+
+
+
+
+// //THIS FUNCTION STARTS THE QUESTION COUNTER AND SCOREBOARD FOR THE TRIVIA 
+// function playGame() {
+//   questionCounter = 0;
+//   score = 0;
+//   availableQuestion = questionBank;
+//   //console.log(availableQuestion);
+//   displayQuestion();
+// };
+// playGame();
+
+
+
+// //final page shows the results: "You scored -- out of --!" 
+
+
+
+
+
+// //set timer (https://www.codegrepper.com/code-examples/javascript/add+countdown+timer+to+javascript+quiz)
+// // var count = 15;
+// // var interval = setInterval(function(){
+// //   document.getElementById('countdown-timer').innerHTML=count;
+// //   count--;
+// //   if (count === 0){
+// //     clearInterval(interval);
+// //     // document.getElementById('countdown-timer').innerHTML='Time is up!'
+// //   }
+// // }, 1000);
