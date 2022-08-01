cd8276d25ee224a04e794c8b0644474af7368a35
shetara2022 <smithshetarall@gmail.com>
2022-08-01 13:53:58 -0400

    add HTML content to Javascript for results and time up page

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const questionBank = [           //QUESTION BANK
     option1: '20',
     option2: '50',
     option3: '15',
-    option4:  '5',
+    option4: '5',
     correctAnswer: 1,
   },
 
@@ -30,28 +30,90 @@ const questionBank = [           //QUESTION BANK
     question: 'What is the capital of North Carolina?',
     option1: 'Charlotte',
     option2: 'Greensboro',
-    option3:  'Asheville',
-    option4:  'Raleigh',
+    option3: 'Asheville',
+    option4: 'Raleigh',
     correctAnswer: 4,
   },
 
   {
     question: 'What is the most common species of bird in the world?',
-    option1: 'pigeon', 
-    option2: 'duck', 
-    option3:  'chicken', 
-    option4:  'blue jay',
+    option1: 'pigeon',
+    option2: 'duck',
+    option3: 'chicken',
+    option4: 'blue jay',
     correctAnswer: 3,
+  },
+
+  {
+    question: 'Area 51 is located in which US state?',
+    option1: 'Wyoming',
+    option2: 'Arizona',
+    option3: 'New Mexico',
+    option4: 'Nevada',
+    correctAnswer: 4,
+  },
+
+  {
+    question: 'How many colors are there in a rainbow?',
+    option1: '6',
+    option2: '7',
+    option3: '8',
+    option4: '9',
+    correctAnswer: 2,
+  },
+
+  {
+    question: 'What is cynophobia the fear of?',
+    option1: 'dogs',
+    option2: 'ducks',
+    option3: 'cats',
+    option4: 'rats',
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'When one is envious, they are said to be what color?',
+    option1: 'red',
+    option2: 'orange',
+    option3: 'green',
+    option4: 'yellow',
+    correctAnswer: 3,
+  },
+
+  {
+    question: 'What is the last letter of the Greek Alphabet?',
+    option1: 'Omega',
+    option2: 'Pi',
+    option3: 'Omicron',
+    option4: 'Zeta',
+    correctAnswer: 1,
+  },
+
+  {
+    question: 'In which year did Facebook launch',
+    option1: '2002',
+    option2: '2003',
+    option3: '2004',
+    option4: '2005',
+    correctAnswer: 3,
+  },
+
+  {
+    question: 'How many notes are there on a standard grand piano?',
+    option1: '68',
+    option2: '72',
+    option3: '78',
+    option4: '88',
+    correctAnswer: 4,
   }
 ]
 
 const timer = document.getElementById('time');
 const question = document.getElementById('question');
 const answerChoice = Array.from(document.getElementsByClassName('answer'));
-// console.log(answerChoice);
-const nextButton = document.getElementById('next-btn');
-const scoreboard = document.getElementById('results');
-const totalQuestions = 5;
+// const nextButton = document.getElementById('next-btn');
+const scoreboard = document.getElementById('total_correct');
+const totalQuestions = 12;
 const questionCounterText = document.querySelector('.hud-main-text');
 
 let currentQuestion = {};
@@ -60,7 +122,7 @@ let availableQuestions = [];
 let score = 0;
 
 
-//after clicking start 
+//start the trivia
 startGame = () => {
   questionCounter = 0;
   score = 0;
@@ -70,26 +132,31 @@ startGame = () => {
 
 //Show Question from question bank
 function displayQuestion() {
-  // if (availableQuestions.length === 0) {
-  //   //show scoreboard
-  // }
+  if (availableQuestions.length === 0 || questionCounter > totalQuestions) {
+    end();
+  }
+ else if (timer === 0) {
+    timeUp();
+  }
+ 
   questionCounter++;
-  questionCounterText.innerText = `${questionCounter}/${totalQuestions}`;
+  questionCounterText.innerText = `${questionCounter} of ${totalQuestions}`;
 
-  const questionIndex = Math.floor(Math.random() * availableQuestions.length); //Code accessed from Stack Overflow(https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array)
+  let questionIndex = Math.floor(Math.random() * availableQuestions.length); //Code accessed from Stack Overflow(https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array)
   currentQuestion = availableQuestions[questionIndex];  //keeps track of which question we are on
   question.innerText = currentQuestion.question; //innerText renders only the text content of an element - (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText)
 
-  //select the number from the dataset property for the corresponding question
+
+  //select the number from the dataset property for the corresponding question to show answer options
   answerChoice.forEach(option => {
     const number = option.dataset['number'];
     option.innerText = currentQuestion['option' + number];
     // console.log(option);
   });
-  
-  availableQuestions.splice(questionIndex, 1);
+  availableQuestions.splice(questionIndex, 1);  //removes questions from the array...
 };
 
+//to click on a response and mark it correct or incorrect 
 answerChoice.forEach(option => {
   option.addEventListener('click', e => {
     const answerSelected = e.target;
@@ -99,106 +166,52 @@ answerChoice.forEach(option => {
     if (selectedAnswer == currentQuestion.correctAnswer) {
       classToApply = 'correct';
     };
-    
     answerSelected.parentElement.classList.add(classToApply);
 
-    setTimeout( () => {
+    setTimeout(() => {                                                   //adds CSS style to buttons 
       answerSelected.parentElement.classList.remove(classToApply);
       displayQuestion();
-    }, 2000);
-
+    }, 1000);
   });
+
+  // //SET COUNTDOWN TIMER 
+  let count = 60;
+  let timer = setInterval(function () {
+    document.getElementById('time').innerHTML = count;
+    count--;
+    if (count === 0) {
+      clearInterval(timer);
+      timeUp();
+      // document.getElementById('time').innerHTML = 'Time is up!';
+    }
+  }, 1000);
 });
-
-
 startGame();
 
+//when trivia has ended 
+function end() {
+  let triviaEndHTML =
+    `
+    <h1>CONGRATULATIONS, You have completed this trivia!</h1>
+    <h2 id = "score"> You scored: ${currentQuestion.correct} of ${totalQuestions}</h2>
+    <div class = "restart">
+    <a href ="index.html">Restart</a>
+    </div>
+    `;
+  let triviaElement = document.getElementById('trivia');
+  triviaElement.innerHTML = triviaEndHTML;
+}
+//if time expires before the player finishes the trivia
+function timeUp() {
+  let timesUpHTML =
+    `
+      <h1>TIME IS UP!</h1>
+      <h2 id = "score"> You scored: ${currentQuestion.correct} of ${totalQuestions}</h2>
+      <div class = "restart">
+       <a href ="index.html">Restart</a>
+    </div>
+      `;
+  let timesUpElement = document.getElementById('trivia');
+  timesUpElement.innerHTML = timesUpHTML;
+}
 
-// // BUTTONS 
-// const answerButton = document.querySelectorAll(".answer");
-// //When I click next button...
-//// let currentQuestion = {};      //select current question     const nextButton = document.getElementById('next-btn');
-// nextButton.addEventListener('click', displayQuestion);  
-
-// const scoreboard = document.getElementById('total_correct');
-// //const time = document.getElementById('time');
-
-
-// //SET TIMER 
-// let count = 15;
-// let timer = setInterval(function(){
-//   document.getElementById('time').innerHTML=count;
-//   count--;
-//   if (count === 0){
-//     clearInterval(timer);
-//     document.getElementById('time').innerHTML = 'Time is up!';
-//     nextButton.click();
-//     displayQuestion()
-//   }
-// }, 1000);
-
-
-
-// function displayQuestion() {         //display random questions
-//   questionCounter++;
-//   const questionIndex = Math.floor(Math.random() * availableQuestion.length); //Code accessed from Stack Overflow(https://stackoverflow.com/questions/43267033/understanding-the-use-of-math-floor-when-randomly-accessing-an-array)
-//   currentQuestion = availableQuestion[questionIndex];  //keeps track of which question we are on
-//   question.innerText = currentQuestion.question; //innerText renders only the text content of an element - (https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText)
-
-//   answerButton.forEach(options => {
-//     const number = options.dataset['number'];
-//     options.innerText = currentQuestion["options" + number];
-//   });
-// answerButton.forEach(function (element, index) {
-//     element.innerText = currentQuestion.options[index];
-// });
-  
-//   availableQuestion.splice(questionIndex, 1);
-  
-//   correctAnswer = true;
-// };
-//     //add event listener to answer buttons
-//     answerButton.forEach(options => {
-//       options.addEventListener('click', e => {
-        
-//       })
-//     })
-//     // nextButton.click();
-//     //   timer = setInterval(timer, 1000);
-// displayQuestion();
-
-// // //correct answer vs incorrect 
-
-
-
-
-
-
-// //THIS FUNCTION STARTS THE QUESTION COUNTER AND SCOREBOARD FOR THE TRIVIA 
-// function playGame() {
-//   questionCounter = 0;
-//   score = 0;
-//   availableQuestion = questionBank;
-//   //console.log(availableQuestion);
-//   displayQuestion();
-// };
-// playGame();
-
-
-
-// //final page shows the results: "You scored -- out of --!" 
-
-
-
-
-
-// //set timer (https://www.codegrepper.com/code-examples/javascript/add+countdown+timer+to+javascript+quiz)
-// // var count = 15;
-// // var interval = setInterval(function(){
-// //   document.getElementById('countdown-timer').innerHTML=count;
-// //   count--;
-// //   if (count === 0){
-// //     clearInterval(interval);
-// //     // document.getElementById('countdown-timer').innerHTML='Time is up!'
-// //   }
-// // }, 1000);
